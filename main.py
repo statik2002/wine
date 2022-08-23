fbf89c5c6266a56310b8eadda8fc818ff3f7f908
statik2002 <statik2002@mail.ru>
2022-08-24 01:02:17 +0800

    добавил правильный вывод года (не доделано)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -9,8 +10,26 @@ env = Environment(
 
 template = env.get_template('template.html')
 
+
+def years_suffix(year):
+    if year % 10 == 1:
+        return 'год'
+    elif year % 10 >= 2 & year % 10 <= 4:
+        return 'года'
+    elif year % 100 >= 11 & year % 100 <= 14:
+        return 'лет'
+    else:
+        return 'лет'
+
+
+foundation_year = 1922
+current_year = datetime.datetime.now().year
+year_delta = current_year-foundation_year
+year_word = years_suffix(year_delta)
+
 rendered_page = template.render(
-    year="100",
+    year=year_delta,
+    year_word=year_word,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
@@ -18,3 +37,6 @@ with open('index.html', 'w', encoding="utf8") as file:
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
+
+
+
