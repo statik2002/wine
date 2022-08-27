cf987bbfac7a6c2dc15837603d1359d6b18c0c48
statik2002 <statik2002@gmail.com>
2022-08-27 12:14:37 +0800

    16

--- excelreader.py
+++ excelreader.py
@@ -2,31 +2,15 @@ import pandas
 import collections
 
 
-# Первая функция чтения из файла из задания (уже не используем, можно удалить)
-def read_from_excel(filename):
-    excel_data = pandas.read_excel(filename, sheet_name='Лист1', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
-
-    return excel_data.to_dict(orient='records')
-
-
-# Вторая функция чтения из файла из задания (уже не используем, можно удалить)
-def read_from_excel2(filename):
-    excel_data2 = pandas.read_excel(filename, sheet_name='Лист1', na_values=None, keep_default_na=False)
-    category = excel_data2.columns.ravel()[0]
-    grouped = excel_data2.groupby(by=category, sort=False).apply(lambda x: x.to_dict(orient='records'))
-    return grouped.to_dict()
-
-
-# Третья функция чтения из файла из задания (рабочая)
-def read_from_excel3(filename):
-    excel_data = pandas.read_excel(filename, sheet_name='Лист1', na_values=None, keep_default_na=False)
-    data_dict = excel_data.to_dict(orient='records')
+def read_products(filepath):
+    data_frame = pandas.read_excel(filepath, sheet_name='Лист1', na_values=None, keep_default_na=False)
+    products = data_frame.to_dict(orient='records')
 
     # Берем название первой колонки из файла
-    category = excel_data.columns.ravel()[0]
+    category = data_frame.columns.ravel()[0]
 
-    final_default_dict = collections.defaultdict(list)
-    for item in data_dict:
-        final_default_dict[item[category]].append(item)
+    product_collection = collections.defaultdict(list)
+    for product in products:
+        product_collection[product[category]].append(product)
 
-    return final_default_dict
+    return product_collection
--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import datetime
-import pprint
-import collections
+import argparse
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from excelreader import read_from_excel, read_from_excel2, read_from_excel3
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-# Функцию read_from_excel3 импортировали из excelreader.py
-wine_data = read_from_excel3('wine3.xlsx')
+from excelreader import read_products
 
 
 # Выводим правильное значение лет\год\года в зависимости от значения года
-def years(year):
+def say_year(year):
     if 11 <= year % 100 <= 19:
         return "лет"
     elif year % 10 == 1:
@@ -30,19 +20,37 @@ def years(year):
         return "лет"
 
 
-foundation_year = 1922
-current_year = datetime.datetime.now().year
-year_delta = current_year - foundation_year
-year_word = years(year_delta)
+def main(filepath, template_path):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-rendered_page = template.render(
-    year=year_delta,
-    year_word=year_word,
-    wine_data=wine_data,
-)
+    template = env.get_template(template_path)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    products_from_excel_file = read_products(filepath)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    foundation_year = 1922
+    current_year = datetime.datetime.now().year
+    year_delta = current_year - foundation_year
+    year_word = say_year(year_delta)
+
+    rendered_page = template.render(
+        year=year_delta,
+        year_word=year_word,
+        wines=products_from_excel_file,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Сайт магазина авторского вина "Новое русское вино"')
+    parser.add_argument('-f', '--filepath', help='Путь к файлу xlsx с винами', default='wine3.xlsx')
+    parser.add_argument('-t', '--templatepath', help='Путь к файлу шаблона', default='template.html')
+    args = parser.parse_args()
+    main(args.filepath, args.templatepath)
