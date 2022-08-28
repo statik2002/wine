0554a33caae4c2d7e514dc9a53651ecc43fbc2e5
statik2002 <statik2002@mail.ru>
2022-08-28 22:31:15 +0800

    вторая правка

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from excelreader import read_products
 
 
 # Выводим правильное значение лет\год\года в зависимости от значения года
-def say_year(year):
+def get_word_year(year):
     if 11 <= year % 100 <= 19:
         return "лет"
     elif year % 10 == 1:
@@ -20,20 +20,26 @@ def say_year(year):
         return "лет"
 
 
-def main(filepath, template_path):
+def main():
+
+    parser = argparse.ArgumentParser(description='Сайт магазина авторского вина "Новое русское вино"')
+    parser.add_argument('-f', '--filepath', help='Путь к файлу xlsx с винами', default='wine.xlsx')
+    parser.add_argument('-t', '--templatepath', help='Путь к файлу шаблона', default='template.html')
+    args = vars(parser.parse_args())
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    template = env.get_template(template_path)
+    template = env.get_template(args['templatepath'])
 
-    products = read_products(filepath)
+    products = read_products(args['filepath'])
 
     foundation_year = 1922
     current_year = datetime.datetime.now().year
     year_delta = current_year - foundation_year
-    year_word = say_year(year_delta)
+    year_word = get_word_year(year_delta)
 
     rendered_page = template.render(
         year=year_delta,
@@ -49,8 +55,4 @@ def main(filepath, template_path):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Сайт магазина авторского вина "Новое русское вино"')
-    parser.add_argument('-f', '--filepath', help='Путь к файлу xlsx с винами', default='wine3.xlsx')
-    parser.add_argument('-t', '--templatepath', help='Путь к файлу шаблона', default='template.html')
-    args = parser.parse_args()
-    main(args.filepath, args.templatepath)
+    main()
